e934d26bbc47031b9827a29bf3e9437c2828e4f3
Efrain Perez Jr <efrainperezjr@live.com>
2018-05-21 01:58:12 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,7 +5,7 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
-    resp = Faraday.get 'https://api.github.com/search' do |req|
+    resp = Faraday.get 'https://api.github.com/search/repositories' do |req|
       req.params['client_id'] = 'my id'
       req.params['client_secret'] = 'my secret'
       req.params['q'] = params[:query]
