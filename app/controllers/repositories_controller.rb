dce6be82e6cd5002cfedcd99fcfd0edf58babd76
Efrain Perez Jr <efrainperezjr@live.com>
2018-05-21 01:56:22 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -5,15 +5,15 @@ class RepositoriesController < ApplicationController
   end
 
   def github_search
-    # resp = Faraday.get 'https://api.github.com/search' do |req|
-    #   req.params['client_id'] = 'my id'
-    #   req.params['client_secret'] = 'my secret'
-    #   req.params['q'] = params[:query]
-    # end
-    #
-    # body_hash = JSON.parse(@resp.body)
-    # @repos = body_hash["items"]
-    #
+    resp = Faraday.get 'https://api.github.com/search' do |req|
+      req.params['client_id'] = 'my id'
+      req.params['client_secret'] = 'my secret'
+      req.params['q'] = params[:query]
+    end
+    
+    body_hash = JSON.parse(@resp.body)
+    @repos = body_hash["items"]
+    
     render 'search'
   end
 end
